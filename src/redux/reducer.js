9281b41c7f57e9c296e94af70185576e11f51e31
MinhTung2504 <minhtungg2504@gmail.com>
2022-08-11 13:41:23 +0700

    Create Store and Root Reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,32 @@
+const initState = {
+  filters: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  todoList: [
+    {
+      id: 1,
+      name: "Learn React",
+      completed: true,
+      priority: "High",
+    },
+    { id: 2, name: "Learn Redux", completed: true, priority: "Medium" },
+    { id: 3, name: "Learn Node", completed: true, priority: "Low" },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "todoList/addTodo":
+      return {
+        ...state,
+        todoList: [...state.todoList, action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
